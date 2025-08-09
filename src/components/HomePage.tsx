ae82cee86b9e0aeb8c2b7517a66dd391c34eb630
Xuemoua(David) <70460701+xuemoua1995@users.noreply.github.com>
2025-08-09 13:00:41 +0700

    Checkpoint for message 2ce90f80-2729-4e83-8514-750acee8eb24

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 import Hero from "./Hero";
 import FlightSearch from "./FlightSearch";
 
-const HomePage = () => {
+const HomePage: React.FC = () => {
   return (
     <div className="bg-white">
       <Navbar />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 
 import HomePage from "../components/HomePage";
 
-const Home = () => {
+const Home: React.FC = () => {
   return (
     <div className="bg-white">
       <HomePage />
